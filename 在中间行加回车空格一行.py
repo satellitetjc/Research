b40b185bbf2a938f415650c96afe75b98a424628
satellitetjc <satellitetjc@163.com>
2017-06-01 15:06:35 +0800

    Create 在中间行加回车空格一行.py

---
+++ 在中间行加回车空格一行.py
@@ -0,0 +1,23 @@
+import sys, string, os
+
+dir = "F:\Training"
+files = os.listdir(dir)
+for f in files:
+        if os.path.splitext(f)[1] == '.txt':
+                
+                Output_Workspace = "F:\Training\mmp"
+                basename = os.path.splitext(f)[0]
+                Output_file = Output_Workspace + os.sep + basename + ".txt"
+
+                if os.path.exists(Output_file) == False:
+                         fin=open(f)
+                         a=fin.readlines()
+                         fout=open(Output_file,'w')
+                         #should write 'w'
+                         #for i in range(0,len(a)-1,3):
+                         #b='\n'.join(a[3:])
+                         b=''.join(a[0:32])+'\n'+''.join(a[32:64])+'\n'+''.join(a[64:96])
+                         #在中间行加回车空格一行
+                         fout.write(b)
+                         fin.close()
+                         fout.close()
