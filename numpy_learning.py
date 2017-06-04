cf1bbbd7a7d2761c137b60a0588b42b6762f4f66
satellitetjc <satellitetjc@163.com>
2017-06-04 21:23:16 +0800

    Update numpy_learning.py

--- numpy_learning.py
+++ numpy_learning.py
@@ -13,6 +13,9 @@ squareDim = 2 * n + 1  #正方形的大小
 
 trainIn = np.zeros((groundTruth.shape[0] - 2 * n, groundTruth.shape[0] - 2 * n, squareDim, squareDim, channels)) #训练的输入
 trainOut = np.zeros((groundTruth.shape[0] - 2 * n, groundTruth.shape[0] - 2 * n)) #训练的输出
+#groundTruth = np.loadtxt(open("E:\\TJC\\2016SMC_China_ascii\\gw1am2_20160100_01m_eqma_l3sgsmchf2210210_chip.csv","rb"),delimiter=",",skiprows=0)
+#obs = np.loadtxt(open("E:\\TJC\\2016LW_China_ascii\\06h\\gw1am2_20160100_01m_eqma_l3sgt06ha2220220_h_chip.csv","rb"),delimiter=",",skiprows=0)  #随机观测值
+
 
 for i in range(n, groundTruth.shape[0] - n):
     for j in range(n, groundTruth.shape[1] - n):
