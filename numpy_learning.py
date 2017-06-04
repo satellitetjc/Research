952da404d0d1adfa9c2eb0ba0841838e390e5778
satellitetjc <satellitetjc@163.com>
2017-06-04 15:30:20 +0800

    Create numpy_learning.py

---
+++ numpy_learning.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+channels = 7  #通道数目
+rows = 100   #行数
+cols = 100   #列数
+
+groundTruth = np.random.random((rows, cols))  # 随机真实值
+
+obs = np.random.random((rows, cols, channels))  #随机观测值
+
+n = 5  #设置扩散的范围，上下左右扩散这么大
+squareDim = 2 * n + 1  #正方形的大小
+
+trainIn = np.zeros((groundTruth.shape[0] - 2 * n, groundTruth.shape[0] - 2 * n, squareDim, squareDim, channels)) #训练的输入
+trainOut = np.zeros((groundTruth.shape[0] - 2 * n, groundTruth.shape[0] - 2 * n)) #训练的输出
+
+for i in range(n, groundTruth.shape[0] - n):
+    for j in range(n, groundTruth.shape[1] - n):
+        trainIn[i - n, j - n, :] = obs[i - n: i + n + 1, j - n: j + n + 1, :]
+        trainOut[i - n, j - n] = groundTruth[i, j]
+        
+trainIn = trainIn.reshape(trainIn.shape[0] * trainIn.shape[1], squareDim, squareDim, channels)
+trainOut = trainOut.reshape(trainOut.shape[0] * trainOut.shape[1], -1)
