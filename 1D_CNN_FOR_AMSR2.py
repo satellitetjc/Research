d216042e392fd034b8e30c56c0810688df0f61c3
satellitetjc <satellitetjc@163.com>
2017-06-08 00:13:00 +0800

    Update 1D_CNN_FOR_AMSR2.py

--- 1D_CNN_FOR_AMSR2.py
+++ 1D_CNN_FOR_AMSR2.py
@@ -29,15 +29,18 @@ def smc_retrieval(channel_size=14, filter_length, nb_input_lw=1, nb_output_smc=1
     # model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['binary_accuracy'])
     return model
  
-def make_lw_pixel(lw_csv,lw_list):
-   for file in os.listdir("E:\\TJC\\2016LW_NC_ASCII\\JULY"):
-      lw_csv[file] = np.loadtxt(open(file,"rb"),delimiter=",",skiprows=0)
-      lw_list[file] = lw_csv[file].reshape(17271) #每个csv文件171*101大小，171*101=1727 
-      trainin = np.zeros((lw_list[1].shape[0],channel_size=14,nb_input_lw=1),dtype=float)
-    for i in range(0,lw_list[1].shape[0]):
-       trainin[i] = [[lw_list[1][i]],[lw_list[2][i]],[lw_list[3][i]],[lw_list[4][i]],[lw_list[5][i]],[lw_list[6][i]]
+def readcsv(lw_csv，lw_list，trainin，channel_size=14,nb_input_lw=1):
+    for channel in os.listdir("E:\\TJC\\2016LW_NC_ASCII\\JULY"):
+      lw_csv[channel] = np.loadtxt(open(file,"rb"),delimiter=",",skiprows=0)
+      lw_list[channel] = lw_csv[channel].reshape(17271) #每个csv文件171*101大小，171*101=1727 
+    trainin = np.zeros((lw_list[1].shape[0],channel_size=14,nb_input_lw=1),dtype=float)
+      for i in range(0,lw_list[1].shape[0]):
+        trainin[i] = [[lw_list[1][i]],[lw_list[2][i]],[lw_list[3][i]],[lw_list[4][i]],[lw_list[5][i]],[lw_list[6][i]]
                       ,[lw_list[7][i]],[lw_list[8][i]],[lw_list[9][i]],[lw_list[10][i]],[lw_list[11][i]],[lw_list[12][i]]
                       ,[lw_list[13][i]],[lw_list[14][i]]]
-      
+    trainin = np.asarry(trainin)
+    x = np.atleast_3d(np.array([trainin[start:start + channel_size] for start in range(0, trainin.shape[0]-channel_size)]))
+    smc = np.loadtxt(open("","rb"),delimiter=",",skiprows=0)
+    y = smc.reshape(17271)
    
 
