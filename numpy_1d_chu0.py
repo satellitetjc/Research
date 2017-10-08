dad8f2fd8c886244e7cd67682ad05b4eba5e3032
satellitetjc <satellitetjc@163.com>
2017-10-08 11:22:33 +0800

    Update numpy_1d_chu0.py

--- numpy_1d_chu0.py
+++ numpy_1d_chu0.py
@@ -2,14 +2,17 @@ import os
 import numpy as np
 import codecs
 #此程序首先将aoi区域二维矩阵转化为一维矩阵，之后去除矩阵中的0值，生成一条有效的数据链，作为神经网络的训练数据，可有效将smc与tb的每个栅格一一对应，输出的是txt格式
-os.chdir("F:\\Training\\amsr2workspace\\")
+os.chdir("F:\\Training\\amsr2workspace\\shuju\\")
+i = 0
 array = np.zeros((264,335),dtype=float)
-array = np.loadtxt(open("gw1am2_20160828_01d_eqma_l3sgsmchf2210210_clip.csv"),delimiter=",",skiprows=0)
-array2 = array.reshape(88440)
-#print array2
-array3 = array2[array2 > 0]
+for i in os.listdir():
+    array = np.loadtxt(open(i,"rb"),delimiter=",",skiprows=0)
+    array2 = array.reshape(64780)
+    #print array2
+    array3 = array2[array2 > 0] 
+    print(array3)
+    f = codecs.open(i+"_chu0.txt","w")
+    file_name = i+"_chu0.txt"
+    np.savetxt(file_name,array3,newline='\n')
 
-print(array3)
-f = codecs.open("gw1am2_20160828_01d_eqma_l3sgsmchf2210210_clip_chu0.txt","w")
-file_name = "gw1am2_20160828_01d_eqma_l3sgsmchf2210210_clip_chu0.txt"
-np.savetxt(file_name,array3,newline='\n')
+
